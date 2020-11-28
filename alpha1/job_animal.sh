2ba9149d37d6f395c941b670b4907643763b7953
SaraSiwiecki <64553171+SaraSiwiecki@users.noreply.github.com>
2020-11-27 19:14:13 -0500

    Add files via upload

---
+++ alpha1/job_animal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=animal_iqtree
+#SBATCH --time=30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+module load IQ-TREE/1.6.12
+iqtree -s alpha1_hydra_precursor_align_clean.fa -bb 1000  -nt AUTO
